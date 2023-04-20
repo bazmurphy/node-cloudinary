1a7e83bc0b958618b84355ee56782cd46f6f3610
Baz Murphy <bazmurphy@gmail.com>
2023-04-20 11:59:21 +0100

    frontend react upload widget

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CloudinaryUploadWidget />
+      <img id="uploadedimage" src=""></img>
+    </div>
+  );
+}
